1b96df2e67ec4c359617eff2149711017787fd28
Lauren Aloia <laurenaloia@gmail.com>
2017-01-16 17:08:58 -0600

    project details

--- js/projects.js
+++ js/projects.js
@@ -6,8 +6,14 @@ var projects ={
 		img: "images/lms.png",
 		img2:"images/lms2.png",
 		img3:"images/lms3.png",
-        name: "Custom LMS", 
+        name: "Custom Learning Management System", 
         details: "Requirements gathering, UX design, content creation, testing ", 
+		overview:"This custom learning management system was developed for The Society of Thoracic Surgeons (STS). I contributed to it for 3 years while employed by STS. It is intended for cardiothoracic surgery residents and faculty members, and it currently has 1,000+ users from 80 different programs. This system allows faculty members to set-up a pre-made curriculum or a custom curriculum for their residents. The system is multi-tenant and contains benchmarked quizzes for comparison across programs. All users have full access to the System's robust Library that features textbooks, videos, case presentations, and more.", 
+		research:"Prior to the development of this system, STS used Moodle for 2 years. When I started my role as the Web Coordinator, I acted as the administrator for Moodle. When we realized Moodle wouldn't suffice as a long-term system, I started to research user needs.  Based on the way Moodle was being used, survey feedback, and a collaborative group, I was able to prioritize functionality and write-up the RFP for a custom LMS.",
+		development:"After receiving proposals and choosing a vendor to develop the LMS, I was the primary contact person at STS. The project manager, software developers and I had many meetings to dive further into the functionality outlined in the scope of work. I created the user flows and many of the wireframes for the system. I also outlined what the usage reports should cover (i.e. 'time spent logged in', 'number of assignments turned in', 'number of quizzes attempted' etc.). ",
+		testing:"I performed a lot of the end-user testing and provided the software developers with feedback.  I also organized a Beta-testing group of ~25 users to test the system and provide me with feedback before the official launch date.",
+		documentation:"I created an online user manual for the system using the GitBook platform."
+		
 		
        
     
